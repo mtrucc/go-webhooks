13c05c59522a2ac8eb93f2f387826015a8526911
mtrucc <mtrucc@qq.com>
2022-01-20 17:47:46 +0800

    一脸楞逼

--- main.go
+++ main.go
@@ -1,28 +1,11 @@
+/*
+Copyright © 2022 NAME HERE <EMAIL ADDRESS>
+
+*/
 package main
 
-import (
-	"github.com/gin-gonic/gin"
-	"log"
-	"net/http"
-)
+import "go-webhooks/cmd"
 
 func main() {
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
-	// For each matched request Context will hold the route definition
-	r.POST("/", func(c *gin.Context) {
-		b, _ := c.GetRawData()
-		log.Println(string(b))
-		c.String(http.StatusOK, "", string(b))
-	})
-
-	err := r.Run()
-	if err != nil {
-		log.Println(err)
-		return
-	} // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	cmd.Execute()
 }
